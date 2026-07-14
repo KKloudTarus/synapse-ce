aa4a43888c7b7981edbf29c522f5d49eb7c82c05
nghiadaulau <nhatnghiatyper@gmail.com>
2026-07-15 00:14:23 +0700

    wip(java): JDBC SQL-concat + Log4Shell JNDI + legacy collection/debug (+7) [toward 450]

--- scripts/langpack/specs_java_deprecations9.py
+++ scripts/langpack/specs_java_deprecations9.py
@@ -21,7 +21,7 @@ RULES = [
       source="https://docs.oracle.com/javase/8/docs/api/java/util/Vector.html", re=r"\.addElement\s*\(", nc="items.addElement(value);", c="items.add(value);"),
     r(id="java-legacy-stack", title="java.util.Stack", desc="Stack is a legacy, synchronized Vector subclass.",
       rationale="java.util.Stack extends Vector and is synchronized; ArrayDeque is preferred.", remediation="Use Deque / ArrayDeque.",
-      source="https://docs.oracle.com/javase/8/docs/api/java/util/Stack.html", re=r"new\s+Stack\s*\(", nc="Stack<Integer> s = new Stack<>();", c="Deque<Integer> s = new ArrayDeque<>();"),
+      source="https://docs.oracle.com/javase/8/docs/api/java/util/Stack.html", re=r"new\s+Stack\s*(<[^>]*>)?\s*\(", nc="Stack<Integer> s = new Stack<>();", c="Deque<Integer> s = new ArrayDeque<>();"),
     r(id="java-thread-dumpstack", title="Thread.dumpStack()", desc="dumpStack is a debugging leftover.",
       rationale="Thread.dumpStack prints to stderr and is usually leftover debugging.", remediation="Remove it, or log with a proper logger.",
       source="https://docs.oracle.com/javase/8/docs/api/java/lang/Thread.html", re=r"Thread\.dumpStack\s*\(", nc="Thread.dumpStack();", c='logger.debug("trace", new Throwable());'),
